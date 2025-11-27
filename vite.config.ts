ccb1165bf49b690c7d5734614be535192ea14284
HeYaoDaDa <3050166231@qq.com>
2025-11-27 11:35:10 +0000

    chore: update GitHub Actions workflow and Vite configuration for improved deployment and build process

--- vite.config.ts
+++ vite.config.ts
@@ -16,4 +16,28 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  build: {
+    chunkSizeWarningLimit: 700,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('vue')) {
+              return 'vue'
+            }
+            if (id.includes('pinia')) {
+              return 'pinia'
+            }
+            if (id.includes('vue-router') || id.includes('vue-i18n')) {
+              return 'vue-routing'
+            }
+            if (id.includes('axios') || id.includes('loglevel')) {
+              return 'utilities'
+            }
+            return 'vendor'
+          }
+        },
+      },
+    },
+  },
 })
